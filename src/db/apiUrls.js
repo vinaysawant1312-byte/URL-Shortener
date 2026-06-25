5ae0f9b16b20719a0526a58aa955c9fb877d0319
vinaysawant1312-byte <vinaysawant1312@gmail.com>
2026-06-25 16:15:12 +0530

    created URL api

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getUrls(user_id) {
   let { data, error } = await supabase
@@ -20,6 +20,44 @@ export async function deleteUrl(id) {
   if (error) {
     console.error(error);
     throw new Error("Unable to delete URLs");
+  }
+
+  return data;
+}
+
+export async function createUrl({
+  title,
+  longurl,
+  customUrl,
+  user_id,
+  qrcode,
+}) {
+  const short_url = Math().random().tostring(36).substring(2, 6);
+  const fileName = `qr-${short_url}`;
+  const { error: storageError } = await supabase.storage
+    .from("qrs")
+    .upload(fileName, qrcode);
+
+  if (storageError) throw new Error(storageError.message);
+
+  const qr = `${supabaseUrl}/storage/v1/object/sign/profile_pic/${fileName}`;
+
+  let { data, error } = await supabase
+    .from("urls")
+    .insert([
+      {
+        title,
+        original_url: longurl,
+        custom_url: customUrl || null,
+        user_id,
+        qrcode,
+      },
+    ])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Error creating short URL");
   }
 
   return data;
